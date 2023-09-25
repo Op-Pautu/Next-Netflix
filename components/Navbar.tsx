2d49ab4ea44cf42a7fde2c6f4c8ae15643089fae
Op-Pautu <pautuop@gmail.com>
2023-09-25 14:28:50 +0530

    Add billboard component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   return (
     <nav className="w-full fixed z-40">
       <div
-        className={`px-4 md:px-16 py-6 flex items-center transition duration-500 ${
+        className={`px-4 md:px-16 py-5 flex items-center transition duration-500 ${
           showBackground ? "bg-zinc-900 bg-opacity-90" : ""
         }`}
       >
@@ -65,10 +65,10 @@ const Navbar = () => {
           <MobileMenu visible={showMobileMenu} />
         </div>
         <div className="flex ml-auto gap-7 items-center">
-          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition text-2xl">
             <BsSearch />
           </div>
-          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition text-xl">
             <BsBell />
           </div>
 
