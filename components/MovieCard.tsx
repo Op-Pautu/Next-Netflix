70be9793b8b9ba61532928fc47e039a71180190d
Op-Pautu <pautuop@gmail.com>
2023-09-25 22:13:34 +0530

    Add functionality and UI for favorite movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
+import FavoriteButton from "./FavoriteButton";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -36,6 +37,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             >
               <BsFillPlayFill size={30} />
             </div>
+            <FavoriteButton movieId={data?.id} />
           </div>
           <p className="text-green-400 font-semibold mt-2">
             New <span className="text-white">2023</span>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth/next";
-import Credentials from "next-auth/providers/credentials";
-import prismadb from '@/lib/prismadb'
-import {compare} from 'bcrypt'
-import GithubProvider from 'next-auth/providers/github' 
-import GoogleProvider from 'next-auth/providers/google' 
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { AuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { compare } from 'bcrypt';
+import prismadb from '@/lib/prismadb';
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID || '',
@@ -17,37 +17,40 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
         Credentials({
-            id: "credentials",
-            name: "Credentials",
+            id: 'credentials',
+            name: 'Credentials',
             credentials: {
                 email: {
                     label: 'Email',
-                    type: "text"
+                    type: 'text',
                 },
                 password: {
                     label: 'Password',
-                    type: 'password'
+                    type: 'passord'
                 }
             },
             async authorize(credentials) {
                 if (!credentials?.email || !credentials?.password) {
-                    throw new Error('Email and password required')
+                    throw new Error('Email and password required');
                 }
+
                 const user = await prismadb.user.findUnique({
                     where: {
                         email: credentials.email
                     }
-                })
+                });
+
                 if (!user || !user.hashedPassword) {
-                    throw new Error("Email does not exist")
+                    throw new Error('Email does not exist');
                 }
 
-                const isCorrectPassword = await compare(credentials.password, user.hashedPassword)
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
                 if (!isCorrectPassword) {
-                    throw new Error("Incorrect Password")
+                    throw new Error('Incorrect password');
                 }
 
-                return user
+                return user;
             }
         })
     ],
@@ -56,12 +59,11 @@ export default NextAuth({
     },
     debug: process.env.NODE_ENV === 'development',
     adapter: PrismaAdapter(prismadb),
-    session: {
-        strategy: 'jwt'
-    },
+    session: { strategy: 'jwt' },
     jwt: {
         secret: process.env.NEXTAUTH_JWT_SECRET,
     },
     secret: process.env.NEXTAUTH_SECRET
-    
-})
+};
+
+export default NextAuth(authOptions);
