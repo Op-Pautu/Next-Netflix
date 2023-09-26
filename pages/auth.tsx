456ab9acddf615a759331d6839c01db081cef966
Op-Pautu <pautuop@gmail.com>
2023-09-26 22:47:10 +0530

    Refactor auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,14 +1,36 @@
-import Input from "@/components/Input";
-import Image from "next/image";
-import { useState, useCallback } from "react";
+/* eslint-disable @next/next/no-img-element */
 import axios from "axios";
-import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
+import Input from "@/components/Input";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 const Auth = () => {
-  const [name, setName] = useState("");
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   const [variant, setVariant] = useState("login");
@@ -24,12 +46,15 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        callbackUrl: "/profiles",
+        redirect: false,
+        callbackUrl: "/",
       });
+
+      router.push("/profiles");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
@@ -38,6 +63,7 @@ const Auth = () => {
         name,
         password,
       });
+
       login();
     } catch (error) {
       console.log(error);
@@ -48,7 +74,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <Image src="/images/logo.png" alt="Logo" width={140} height={140} />
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
@@ -58,46 +84,46 @@ const Auth = () => {
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
-                  label="Username"
-                  onChange={(e: any) => setName(e.target.value)}
                   id="name"
+                  type="text"
+                  label="Username"
                   value={name}
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
-
               <Input
-                label="Email"
-                onChange={(e: any) => setEmail(e.target.value)}
                 id="email"
                 type="email"
+                label="Email address or phone number"
                 value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                label="Password"
-                onChange={(e: any) => setPassword(e.target.value)}
-                id="password"
                 type="password"
+                id="password"
+                label="Password"
                 value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
             <button
               onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
-              {variant === "login" ? "Login" : "Sign Up"}
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
                 onClick={() => signIn("google", { callbackUrl: "/profiles" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
-                <FcGoogle size={30} />
+                <FcGoogle size={32} />
               </div>
               <div
                 onClick={() => signIn("github", { callbackUrl: "/profiles" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
-                <FaGithub size={30} />
+                <FaGithub size={32} />
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
@@ -110,6 +136,7 @@ const Auth = () => {
               >
                 {variant === "login" ? "Create an account" : "Login"}
               </span>
+              .
             </p>
           </div>
         </div>
